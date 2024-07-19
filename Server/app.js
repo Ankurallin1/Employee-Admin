e757f6540c2f3829eab9bc8614fff0dfac0026b6
Ankur Srivastava <ankursrivastava2070@gmail.com>
2024-07-19 11:20:31 +0530

    Throttling and rate limited added and disable source map

--- Server/app.js
+++ Server/app.js
@@ -2,8 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const rateLimit = require('express-rate-limit');
+const slowDown = require('express-slow-down');
 const cors = require('cors');
 const port = 5000;
+const rateLimit = require('express-rate-limit');
+const slowDown = require('express-slow-down');
 const Router_crud = require('./Routes/Router');
 const UserLogin = require('./Routes/userRoutes')
 require('./DB/Connect');
@@ -13,6 +17,21 @@ app.use(cors(
         credentials: true,
     }
 ));
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, 
+    max: 100, 
+    message: 'Too many requests, please try again later.'
+});
+
+const speedLimiter = slowDown({
+    windowMs: 15 * 60 * 1000, 
+    delayAfter: 50, 
+    delayMs: 500 
+});
+
+app.use(limiter); 
+app.use(speedLimiter);
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
